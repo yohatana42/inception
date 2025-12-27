aea0990dba574c66928441ff2ef34631cb067e57
Yoshiko Hatanaka <yohatana@c6r3s5.42tokyo.jp>
2025-12-27 15:07:24 +0900

    nginx alone build

---
+++ srcs/nginx/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "== set up nginx =="
+mkdir -p /etc/nginx/ssl
+
+echo "create public key for nginx"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout /etc/nginx/ssl/nginx.key \
+	-out /etc/nginx/ssl/nginx.crt \
+	-subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42/OU=Inception/CN=yohatana.42.fr"
+
+echo "start nginx..."
+# バックグラウンド処理だと終了後コンテナが落ちる
+nginx -g 'daemon off;'
